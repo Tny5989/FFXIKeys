a727bd4197c2bc293e6ca9510e7eb1176b82f7b3
Tony <admassarini@gmail.com>
2018-10-18 12:41:25 -0500

    update locks list

--- data/locks.lua
+++ data/locks.lua
@@ -1,13 +1,13 @@
 local Locks = {}
-Locks['habitox'] = 'Habitox'
-Locks['mystrix'] = 'Mystrix'
-Locks['Bountibox'] = 'Bountibox'
-Locks['specilox'] = 'Specilox'
-Locks['arbitrix'] = 'Arbitrix'
-Locks['funtrox'] = 'Funtrox'
-Locks['sweepstox'] = 'Sweepstox'
-Locks['priztrix'] = 'Priztrix'
-Locks['wondrix'] = 'Wondrix'
-Locks['rewardox'] = 'Rewardox'
-Locks['winrix'] = 'Winrix'
+Locks['habitox'] = 0
+Locks['mystrix'] = 0
+Locks['Bountibox'] = 0
+Locks['specilox'] = 0
+Locks['arbitrix'] = 0
+Locks['funtrox'] = 0
+Locks['sweepstox'] = 17780998
+Locks['priztrix'] = 0
+Locks['wondrix'] = 0
+Locks['rewardox'] = 0
+Locks['winrix'] = 0
 return Locks
