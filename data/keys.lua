0e09cdd6fb0c55ae6a22e528b98c24971e253ac0
Tony <admassarini@gmail.com>
2019-01-03 23:41:22 -0600

    pad value

--- data/keys.lua
+++ data/keys.lua
@@ -31,8 +31,8 @@ Keys.Values = {}
 Keys.Values['']   = { id = 0000, idx = 0, en = '' }
 
 -- Keys
-Keys.Values[8973] = { id = 8973, idx = 1, en = 'SP Gobbie Key' }
-Keys.Values[4181] = { id = 4181, idx = 2, en = 'Warp Scroll' }
+Keys.Values[8973] = { id = 8973, idx = 01, en = 'SP Gobbie Key' }
+Keys.Values[4181] = { id = 4181, idx = 02, en = 'Warp Scroll' }
 Keys.Values[5945] = { id = 5945, idx = 12, en = "Prize Powder" }
 
 --------------------------------------------------------------------------------
