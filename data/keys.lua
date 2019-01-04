d643af40975dc8f9bfd6b9fcebc79bc1450431b1
Tony <admassarini@gmail.com>
2019-01-03 23:54:28 -0600

    replace double quotes with single quotes

--- data/keys.lua
+++ data/keys.lua
@@ -33,7 +33,7 @@ Keys.Values['']   = { id = 0000, idx = 0, en = '' }
 -- Keys
 Keys.Values[8973] = { id = 8973, idx = 01, en = 'SP Gobbie Key' }
 Keys.Values[4181] = { id = 4181, idx = 02, en = 'Warp Scroll' }
-Keys.Values[5945] = { id = 5945, idx = 12, en = "Prize Powder" }
+Keys.Values[5945] = { id = 5945, idx = 12, en = 'Prize Powder' }
 
 --------------------------------------------------------------------------------
 function Keys.GetByProperty(key, value)
